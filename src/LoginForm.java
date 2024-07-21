954ee1909ce8bcc63d9b37f47122e60f083a96a5
dul <eadndp@gmail.com>
2024-07-21 10:24:11 +0530

    ins

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -2,6 +2,12 @@ import javax.swing.*;
 
 public class LoginForm extends JFrame{
     public void initialize(){
-        
+        setTitle("Login Form");
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setSize(400, 500);
+        setMinimumSize(new Dimension(350, 450));
+        //setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 }

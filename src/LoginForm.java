05da589ae15101f458285babdc6f3009bd9ca791
dul <eadndp@gmail.com>
2024-07-30 15:53:39 +0530

    formt

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -2,42 +2,41 @@ import java.awt.*;
 
 import javax.swing.*;
 
-public class LoginForm extends JFrame{
-    final private Font mainFont = new Font ("Segoe print", Font.BOLD, 18);
-    
-    public void initialize(){
-        /*Form Panel */
+public class LoginForm extends JFrame {
+    final private Font mainFont = new Font("Segoe print", Font.BOLD, 18);
 
+    public void initialize() {
+        /* Form Panel */
 
-JLabel lbLoginForm = new JLabel("Login Form", SwingConstants.CENTER);
-lbLoginForm.setFont(mainFont);
-JTextField tfEmail;
-JPasswordField pfPassword;
+        JLabel lbLoginForm = new JLabel("Login Form", SwingConstants.CENTER);
+        lbLoginForm.setFont(mainFont);
+        JTextField tfEmail;
+        JPasswordField pfPassword;
 
-JLabel lbEmail = new JLabel("Email");
-lbEmail.setFont(mainFont);
+        JLabel lbEmail = new JLabel("Email");
+        lbEmail.setFont(mainFont);
 
-tfEmail = new JTextField();
-tfEmail.setFont(mainFont);
+        tfEmail = new JTextField();
+        tfEmail.setFont(mainFont);
 
-JLabel lbPassword = new JLabel("Password");
-lbPassword.setFont(mainFont);
+        JLabel lbPassword = new JLabel("Password");
+        lbPassword.setFont(mainFont);
 
-pfPassword = new JPasswordField();
-pfPassword.setFont(mainFont);
+        pfPassword = new JPasswordField();
+        pfPassword.setFont(mainFont);
 
-JPanel formPanel = new JPanel();
-formPanel.setLayout(new GridLayout(0, 1, 10, 10));
-formPanel.add(lbLoginForm);
-formPanel.add(lbEmail);
-formPanel.add(tfEmail);
-formPanel.add(lbPassword);
-formPanel.add(pfPassword);
+        JPanel formPanel = new JPanel();
+        formPanel.setLayout(new GridLayout(0, 1, 10, 10));
+        formPanel.add(lbLoginForm);
+        formPanel.add(lbEmail);
+        formPanel.add(tfEmail);
+        formPanel.add(lbPassword);
+        formPanel.add(pfPassword);
         setTitle("Login Form");
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setSize(400, 500);
         setMinimumSize(new Dimension(350, 450));
-        //setResizable(false);
+        // setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
     }

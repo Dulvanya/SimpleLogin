a9a54bca0faa4c35e834890f01492ceba268dcf8
dul <eadndp@gmail.com>
2024-12-11 15:40:24 +0530

    a

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -1,8 +1,28 @@
+import java.awt.*;
+
 import javax.swing.*;
 
-public class LoginForm extends JFrame{
-    public void initialize(){
-        
+public class LoginForm extends JFrame {
+final private Font mainFont = new Font("Segeo print", Font.BOLD, 18);
+
+    public void initialize() {
+        /************ From pael ************/
+JLabel lbLoginForm = new JLabel("Login Form", SwingConstants.CENTER);
+lbLoginForm.setFont(mainFont);
+
+JLabel lbEmail = new JLabel("Email");
+lbEmail.setFont(mainFont);
+
+
+
+
+        setTitle("Logi Form");
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setSize(400, 500);
+        setMinimumSize(new Dimension(350, 450));
+        // setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
-    
+
 }

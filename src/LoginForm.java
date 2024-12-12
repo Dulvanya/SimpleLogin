a1f1606a5a1473c7c0d66a161e11195ab2b218dd
dul <eadndp@gmail.com>
2024-12-12 18:56:51 +0530

    n

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 public class LoginForm extends JFrame {
 final private Font mainFont = new Font("Segeo print", Font.BOLD, 18);
 JTextField tfEmail;
+JPasswordField pfPassword;
+JFrame formPanel;
 
     public void initialize() {
         /************ From pael ************/
@@ -15,7 +17,32 @@ JLabel lbEmail = new JLabel("Email");
 lbEmail.setFont(mainFont);
 
 
+tfEmail = new JTextField();
+tfEmail.setFont(mainFont);
 
+JLabel lbPassword = new JLabel("password");
+lbPassword.setFont(mainFont);
+
+pfPassword = new JPasswordField();
+pfPassword.setFont(mainFont);
+
+JPanel formJPanel = new JPanel();
+formPanel.setLayout(new GridLayout(0, 1, 10, 10));
+formJPanel.add(lbLoginForm);
+formPanel.add(lbEmail);
+formPanel.add(tfEmail);
+formPanel.add(lbPassword);
+formPanel.add(pfPassword);
+
+
+/*Buttons Panel */
+
+
+
+
+/*Initialise the frame */
+
+add(formPanel, BorderLayout.NORTH);
 
         setTitle("Logi Form");
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);

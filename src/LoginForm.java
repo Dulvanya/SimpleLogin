a47e36c28592d7e964e76f60ddefa364f411942b
dul <eadndp@gmail.com>
2024-12-18 08:08:40 +0530

    mainform

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -1,6 +1,10 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.swing.*;
 
@@ -11,6 +15,9 @@ public class LoginForm extends JFrame {
     JFrame formPanel;
 
     public void initialize() {
+
+
+        
         /************ From pael ************/
 JLabel lbLoginForm = new JLabel("Login Form", SwingConstants.CENTER);
 lbLoginForm.setFont(mainFont);
@@ -66,9 +73,29 @@ else{
 
 });
 
+
+JButton btnCancel = new JButton("Cancel");
+btnCancel.setFont(mainFont);
+btnCancel.addActionListener(new ActionListener(){
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        dispose();
+         }
+
+    
+});
+
+JPanel buttonsPanel = new JPanel();
+buttonsPanel.setLayout(new GridLayout(1, 2, 10, 0));
+buttonsPanel.add(btnLogin);
+buttonsPanel.add(btnCancel);
+
 /*Initialise the frame */
 
 add(formPanel, BorderLayout.NORTH);
+add(buttonsPanel, BorderLayout.SOUTH);
 
         setTitle("Logi Form");
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -79,12 +106,42 @@ add(formPanel, BorderLayout.NORTH);
         setVisible(true);
     }
 
+    private User getAuthenticationUser(String email, String password) {
+        User user = null;
 
+        final String DB_URL = " ";
+        final String USERNAME = "root";
+        final String PASSWORD = " ";
 
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
 
+            String sql = "SELECT * FROM users WHERE email=? AND password=?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
 
+            ResultSet resultSet = preparedStatement.executeQuery();
 
-    
+            if (resultSet.next()) {
+                user = new User();
+                user.name = resultSet.getString("name");
+                user.email = resultSet.getString("email");
+                user.phone = resultSet.getString("phone");
+                user.address = resultSet.getString("address");
+                user.password = resultSet.getString("password");
+            }
+
+            preparedStatement.close();
+            conn.close();
+
+        }
+
+        catch (Exception e) {
+            System.out.println("Databse connection failed");
+        }
+
+        return user;
+    }
+
 }
-
-
